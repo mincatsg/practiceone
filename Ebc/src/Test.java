1ef6440bc44ae82b59adeadb6ff9004e59d8d1d4
mincatsg <1374723981@qq.com>
2019-12-07 22:46:16 +0800

    练习

---
+++ Ebc/src/Test.java
@@ -0,0 +1,46 @@
+  interface MM{
+	  public void mianji();
+  }
+  
+  
+ public class yuan implements MM{
+	   double t=3.14;
+	   int r;
+	  public void mianji(){
+		  double w;
+		  w=t*r*r;
+      System.out.println("圆的面积:"+w);
+	  }
+ }
+	 
+ public class juxing implements MM{
+	   int a;
+	   int b;
+	   public void get(int z,int x){
+		   a=z;
+		   b=x;
+	   }
+	  public void mianji(){
+		  int s;
+		  s=a*b;
+	System.out.println("矩形面积:"+s);
+	  }
+	 public class Test{
+		 public  void main(String args[]){
+			 yuan yuan1=new yuan();
+			 yuan1.r=2;
+			 yuan1.mianji();
+			 juxing juxing1=new juxing();
+			 juxing1.get=(6,7);
+			 juxing1.mianji();	 
+		 }
+	 }
+	 
+	   
+	 
+ }
+	  
+	 
+	 
+	   
+	   
