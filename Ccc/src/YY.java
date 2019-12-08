ab4d829f235b773fa718d4400e548160f9205908
mincatsg <1374723981@qq.com>
2019-12-08 17:43:15 +0800

    2

---
+++ Ccc/src/YY.java
@@ -0,0 +1,47 @@
+
+public class YY {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int k = 10 ;
+		int[] num = new int[k];
+		for(int i = 0;i<num.length;i++){
+		num[i] = rodem();
+		}
+		print(num);
+		}
+		public static int rodem(){
+		int i = (int)(Math.random()*100) ;
+		if(i>50){
+		return i ;
+		}
+		return rodem();
+		}
+		public static void print(int[] n){
+		for(int i = 0;i<n.length;i++){
+		if(i != 0){
+		System.out.print(",") ;
+		}
+		System.out.print(n[i]) ;
+		}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
