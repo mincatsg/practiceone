21cb2949c0ec1b9f72f172b1abb8b563adb0e2d6
mincatsg <1374723981@qq.com>
2019-12-12 20:14:38 +0800

    3

---
+++ package1105/MainTest00.java
@@ -0,0 +1,13 @@
+package package1105;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class MainTest00 {
+
+    public static void main(String[] args) {
+
+
+    }
+}
