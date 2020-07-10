f06870335cac8ff18e10729f438a27a9537fb681
mincatsg <1374723981@qq.com>
2020-07-10 21:27:12 +0800

    3

---
+++ test10.java
@@ -0,0 +1,15 @@
+
+
+   public class test10 {
+    private float f = 1.0f;
+    int m = 12;
+    static int n = 1;
+    public static void main (String args[]) {
+        test10 t = new test10();
+
+//        test10.f = 10;
+//        t.f = 10; //对
+//        this.n = 10;
+//        test10.m = 10;
+    }
+}
